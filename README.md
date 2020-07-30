06bcdf148827b0e8dfb1ae38458c7eb54efb43a2
DaJuan Harris <dajuancancode@gmail.com>
2020-07-30 18:29:31 -0400

    Work with generics

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 
 In this repository, you will find all the code talked about in the TypeScript Workshop. Each section taught in the workshop is broken up into it's own branch, with the master branch reperesenting all files
 
-- Setup
-  - Installing TypeScript
-  - Configuring TypScript with a tsconfig.json file
-  - Installing ESlint
-  - Configuring ESlint with .eslintrc and .eslintignore files
-  - Installing Prettier
-  - Configuring Prettier with a prettierrc file
-  - Installing nodemon, rimraf, and ts-node
-  - writing all scripts and configuring nodemon with nodemon.json
+- Generics
+  - Generic functions without Generics
+  - Generic functions with Generics
+  - Interfaces and Generics
--- build/Lesson1.js
+++ build/Lesson1.js
@@ -1,42 +1,22 @@
+"use strict";
 // Generics give us the ability to abstract types for resuable blocks of code
-
 // Here is a scenario, where we have a function, that we want to work with any value. We could use the any type
 // function genericTest(arg: any): any {
 //   return arg
 // }
-
 // any is generic enough to cause the function to accpet any and all types
 // However, with the use of any, we are loosing out on data about the argument
-
 // console.log(genericTest(3));
 // console.log(genericTest("Hello World"));
-
 // T is a type variable that can represent any type
-function genericTest<T>(arg: T): T {
-  return arg;
+function genericTest(arg) {
+    return arg;
 }
-
-console.log(genericTest<string>("Hello World"));
-console.log(genericTest<number>(3));
-
-interface Resource<T> {
-  uid: number;
-  resourceName: string;
-  data: T;
-}
-
-const doc1: Resource<string[]> = {
-  uid: 1,
-  resourceName: "shoppingList",
-  data: ["bread", "milk"]
+console.log(genericTest("Hello World"));
+console.log(genericTest(3));
+var doc1 = {
+    uid: 1,
+    resourceName: "shoppingList",
+    data: ["bread", "milk"]
 };
-
 console.log(doc1);
-
-const doc2: Resource<{ name: string; major: string }> = {
-  uid: 1,
-  resourceName: "student",
-  data: { name: "DaJuan", major: "Computer Science" }
-};
-
-console.log(doc2);
